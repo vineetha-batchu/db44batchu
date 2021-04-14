8c5ade21a5a890e1d809805dfe633b66ee8d3673
vineetha-batchu <77755310+vineetha-batchu@users.noreply.github.com>
2021-04-13 23:07:52 -0500

    assignment 12

--- controllers/jeep.js
+++ controllers/jeep.js
@@ -6,15 +6,15 @@ exports.jeep_list = async function (req, res) {
         res.send(theJeeps);
     }
     catch (err) {
-        res.send( `{"error": ${err}}`);
+        res.send(`{"error": ${err}}`);
         res.status(500);
     }
 };
 // for a specific Jeep.
 exports.jeep_detail = async function (req, res) {
-    console.log("detail"  + req.params.id)
+    console.log("detail" + req.params.id)
     try {
-        result = await Jeep.findById( req.params.id)
+        result = await Jeep.findById(req.params.id)
         res.send(result)
     } catch (error) {
         res.status(500)
@@ -42,18 +42,27 @@ exports.jeep_create_post = async function (req, res) {
         res.status(500);
     }
 };// Handle Jeep delete form on DELETE.
-exports.jeep_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Jeep delete DELETE ' + req.params.id);
+exports.jeep_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Jeep.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+
 };
 // Handle Jeep update form on PUT.
 exports.jeep_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
     try {
-        let toUpdate = await Jeep.findById( req.params.id)
+        let toUpdate = await Jeep.findById(req.params.id)
         // Do updates of properties
-        if(req.body.jeepname) toUpdate.jeepname = req.body.jeepname;
-        if(req.body.enginemodel) toUpdate.enginemodel = req.body.enginemodel;
-        if(req.body.price) toUpdate.price = req.body.price;
+        if (req.body.jeepname) toUpdate.jeepname = req.body.jeepname;
+        if (req.body.enginemodel) toUpdate.enginemodel = req.body.enginemodel;
+        if (req.body.price) toUpdate.price = req.body.price;
         let result = await toUpdate.save();
         console.log("Sucess " + result)
         res.send(result)
@@ -76,3 +85,59 @@ exports.jeep_view_all_Page = async function (req, res) {
         res.status(500);
     }
 };
+// Handle a show one view with id specified by query
+exports.jeep_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Jeep.findById(req.query.id)
+        res.render('jeepdetail',
+            { title: 'jeep Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.jeep_create_Page =  function(req, res) {
+    console.log("create view")
+    try{
+        res.render('jeepcreate', { title: 'Jeep Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a costume.
+// query provides the id
+exports.jeep_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Jeep.findById(req.query.id)
+        res.render('jeepupdate', { title: 'Jeep Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.jeep_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await Jeep.findById(req.query.id)
+        res.render('jeepdelete', { title: 'Jeep Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+
+
+
--- routes/jeeps.js
+++ routes/jeeps.js
@@ -4,4 +4,8 @@ const jeep_controlers= require('../controllers/jeep');
 
 /* GET home page. */
 router.get('/', jeep_controlers.jeep_view_all_Page );
+router.get('/detail', jeep_controlers.jeep_view_one_Page);
+router.get('/create', jeep_controlers.jeep_create_Page);
+router.get('/update', jeep_controlers.jeep_update_Page);
+router.get('/delete', jeep_controlers.jeep_delete_Page);
 module.exports = router;
