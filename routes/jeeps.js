ea317eda9f643dc2aa40b4bc58eeab42ecda3524
vineetha-batchu <77755310+vineetha-batchu@users.noreply.github.com>
2021-04-23 14:05:17 -0500

    bonus protected endpoints

--- routes/jeeps.js
+++ routes/jeeps.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 const jeep_controlers = require('../controllers/jeep');
 
-/* GET home page. */
-router.get('/', jeep_controlers.jeep_view_all_Page);
-router.get('/detail', jeep_controlers.jeep_view_one_Page);
-router.get('/create', jeep_controlers.jeep_create_Page);
 // A little function to check if we have an authorized user and continue on
 //or
 // redirect to login.
@@ -16,6 +12,11 @@ const secured = (req, res, next) => {
     req.session.returnTo = req.originalUrl;
     res.redirect("/login");
 }
+
+/* GET home page. */
+router.get('/', jeep_controlers.jeep_view_all_Page);
+router.get('/detail', jeep_controlers.jeep_view_one_Page);
+router.get('/create',secured, jeep_controlers.jeep_create_Page);
 router.get('/update', secured, jeep_controlers.jeep_update_Page);
-router.get('/delete', jeep_controlers.jeep_delete_Page);
+router.get('/delete',secured, jeep_controlers.jeep_delete_Page);
 module.exports = router;
